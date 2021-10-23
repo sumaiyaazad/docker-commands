ce8aaf3d6da1f3214432f3ac59c0a85c6bb8af1c
Mahathir <mirmahathir1@gmail.com>
2021-10-23 12:05:50 +0600

    added force removal of containers

--- dockerreset.sh
+++ dockerreset.sh
@@ -1,4 +1,13 @@
+echo "CLEANING UP YOUR DOCKER"
+echo "---------remove services-----------"
 docker service rm $(docker service ls -q)
-docker rm -vf $(docker ps -a -q)
+echo "---------remove containers-----------"
+docker container rm -f $(docker ps -a -q)
+echo "---------remove volumes-----------"
+docker volume rm $(docker volume ls -q)
+echo "---------remove images-----------"
 docker rmi -f $(docker images -a -q)
+echo "---------remove networks-----------"
 docker network prune
+
+
